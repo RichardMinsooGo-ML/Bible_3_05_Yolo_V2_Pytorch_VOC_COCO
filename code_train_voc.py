fe2486494f5676f9aadd9ceeb39fa556ec586129
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-06-15 09:39:00 +0900

    Update code_train_voc.py

--- code_train_voc.py
+++ code_train_voc.py
@@ -74,7 +74,7 @@ def train(opt):
             # model.load_state_dict(torch.load(opt.pre_trained_model_path))
     else:
         if opt.pre_trained_model_type == "model":
-            # model = torch.load(opt.pre_trained_model_path, map_location=lambda storage, loc: storage)
+            model = torch.load(opt.pre_trained_model_path, map_location=lambda storage, loc: storage)
         else:
             model = Yolo(training_set.num_classes)
             # model.load_state_dict(torch.load(opt.pre_trained_model_path, map_location=lambda storage, loc: storage))
